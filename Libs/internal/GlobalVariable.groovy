762e17f5a6c86814180dca8cf7cf509a62a0c97b
jether <arcillasjether@gmail.com>
2018-08-29 18:21:16 +0800

    ADD: Authentication Test Cases (1-5)

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,45 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p>Profile Staging : username used to log in</p>
+     */
+    public static Object Gusername
+     
+    /**
+     * <p>Profile Staging : password for nora-qa</p>
+     */
+    public static Object Gpassword
+     
+    /**
+     * <p></p>
+     */
+    public static Object GloginUrl
+     
+
+    static {
+        def allVariables = [:]        
+        allVariables.put('default', [:])
+        allVariables.put('Staging', allVariables['default'] + ['Gusername' : 'nora-qa', 'Gpassword' : 'Vf4WmEw!', 'GloginUrl' : 'http://apsite-staging.hidora.com/login.html'])
+        
+        String profileName = RunConfiguration.getExecutionProfile()
+        
+        def selectedVariables = allVariables[profileName]
+        Gusername = selectedVariables['Gusername']
+        Gpassword = selectedVariables['Gpassword']
+        GloginUrl = selectedVariables['GloginUrl']
+        
+    }
+}
---
+++ Object Repository/Login/a_Login.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Login</name>
+   <tag></tag>
+   <elementGuidId>8a66de76-44b6-47e7-8c0c-f0c127850d21</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>login.html</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>login-btn signin hide-loggedin</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ap-translate</name>
+      <type>Main</type>
+      <value>HOME_DIRECT.LOGIN</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Login</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;domloaded&quot;]/body[@class=&quot;loggedout&quot;]/header[@class=&quot;header&quot;]/div[@class=&quot;main-container&quot;]/div[@class=&quot;header-inner&quot;]/div[@class=&quot;row w-100&quot;]/div[@class=&quot;col-8&quot;]/div[@class=&quot;wrap&quot;]/div[@class=&quot;login&quot;]/a[@class=&quot;login-btn signin hide-loggedin&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
