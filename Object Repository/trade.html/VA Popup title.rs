dbc4d5f41f06312d77ee8d9ab902002ed6d4ccf7
arcillas <arcillas@arcillas-PC>
2018-09-03 19:38:19 +0800

    ADD: Add new test case DQRXD-146 and Object Repository

---
+++ Object Repository/trade.html/VA Popup title.rs
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>VA Popup title</name>
+   <tag></tag>
+   <elementGuidId>a2ba79f6-cfff-454f-a4f9-078af98dcdc6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;trade-deposit-dialog&quot;)/div[@class=&quot;ap-modal_inner sc-dxgOiQ jPwguh&quot;]/div[2]/div[1]/div[1]/div[@class=&quot;ap-header&quot;][count(. | //*[@class = 'ap-header']) = count(//*[@class = 'ap-header'])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ap-header</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;trade-deposit-dialog&quot;)/div[@class=&quot;ap-modal_inner sc-dxgOiQ jPwguh&quot;]/div[2]/div[1]/div[1]/div[@class=&quot;ap-header&quot;]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>trade-deposit-dialog</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/trade.html/Verify Account Link text.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Verify Account Link text</name>
+   <tag></tag>
+   <elementGuidId>ad9cb4ec-b88b-48de-b15f-21f1d09929b1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/div[1]/div[1]/ul/li[5]/a</value>
+   </webElementProperties>
+</WebElementEntity>
