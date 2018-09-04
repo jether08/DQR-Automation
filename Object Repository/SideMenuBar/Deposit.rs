e54b6b19434f7ec5d2292c10b7574c0245197ee7
arcillas <arcillas@arcillas-PC>
2018-09-04 20:36:35 +0800

    ADD: Add new testcase DQRXD-144

---
+++ Object Repository/SideMenuBar/Deposit.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Deposit</name>
+   <tag></tag>
+   <elementGuidId>7249b510-2e4a-466f-9c68-1592542ae7b3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/div[1]/div[1]/ul/li[1]/a</value>
+   </webElementProperties>
+</WebElementEntity>
