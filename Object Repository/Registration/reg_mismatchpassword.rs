09f2d77ce989ed324423aa88c36dd812a6e00cc1
arcillas <arcillas@arcillas-PC>
2018-09-09 15:15:05 +0800

    ADD: Add test case DQRXD-153 SC2, Edit Test case DQRXD-153 SC1

---
+++ Object Repository/Registration/reg_mismatchpassword.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>reg_mismatchpassword</name>
+   <tag></tag>
+   <elementGuidId>ab21b4a5-778f-44ad-891f-968cf03f6f0c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;registerform&quot;]/div/div/form/div[1]/div[4]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;registerform&quot;]/div/div/form/div[1]/div[4]/div</value>
+   </webElementProperties>
+</WebElementEntity>
