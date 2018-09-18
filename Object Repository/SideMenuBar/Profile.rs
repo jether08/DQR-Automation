8ad8da85363349573c91619e30029cfa5151cf1e
arcillas <arcillas@arcillas-PC>
2018-09-19 06:42:19 +0800

    ADD: Add new test case Side3

---
+++ Object Repository/SideMenuBar/Profile.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Profile</name>
+   <tag></tag>
+   <elementGuidId>ba0d5027-359f-4275-b7bf-37ddf61e8db1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[contains(text(), 'Profile')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[contains(text(), 'Profile')]</value>
+   </webElementProperties>
+</WebElementEntity>
