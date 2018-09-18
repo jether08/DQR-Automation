4f1f13beeadaf57feb7290f2eb1d58bee096cd6d
nora <arcillasjether@gmail.com>
2018-09-18 21:59:49 +0800

    ADD: SIDE5 Test Case

---
+++ Object Repository/SideMenuBar/Signout.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Signout</name>
+   <tag></tag>
+   <elementGuidId>8aa58e69-03db-46a8-8b2f-deb947ebb8bb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[contains(text(), 'Sign Out')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[contains(text(), 'Sign Out')]</value>
+   </webElementProperties>
+</WebElementEntity>
--- Scripts/SIDE5/Script1537276316209.groovy
+++ Scripts/SIDE5/Script1537276316209.groovy
@@ -12,40 +12,18 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.By
-import org.openqa.selenium.WebElement
-import org.openqa.selenium.WebDriver
-//import org.openqa.selenium.WebElement
-import org.openqa.selenium.support.ui.Select
-import com.kms.katalon.core.webui.driver.DriverFactory
-import com.kms.katalon.core.util.KeywordUtil
-import com.kms.katalon.core.logging.KeywordLogger
 
-//Call Login custom keyword
-CustomKeywords.'utility.ValidLogin.Level0Login'()
+
+//Call Level4 Login custom keyword
+CustomKeywords.'utility.ValidLogin.Level4Login'()
 
 WebUI.delay(10)
-
-//Initializing webdriver script
-WebDriver driver = DriverFactory.getWebDriver()
 
 //Click Hamburger Icon
 WebUI.click(findTestObject('trade.html/Hamburger Menu'))
 
-
-driver.findElement(By.xpath("//*[contains(text(), 'Sign Out')]")).click()
-
-
-WebUI.navigateToUrl('https://test.dqr-private.com/trade.html')
-
-
-WebUI.click(findTestObject('trade.html/Hamburger Menu'))
-
-WebUI.verifyElementPresent(findTestObject('SideMenuBar/OpenSideMenu'), 5, FailureHandling.STOP_ON_FAILURE)
-
-
-println('Side Menu is Open Successfuly')
-
+//Verify Signout button is clickable'
+WebUI.verifyElementClickable(findTestObject('SideMenuBar/Signout'))
 
 
 
