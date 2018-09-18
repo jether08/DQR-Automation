9b61bf9c0abb3eaa93eabc9f07bdbedba8ba3417
arcillas <arcillas@arcillas-PC>
2018-09-19 07:20:01 +0800

    ADD: Add new test case Side6

---
+++ Object Repository/SideMenuBar/Help.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Help</name>
+   <tag></tag>
+   <elementGuidId>01d4c44f-6ba0-42b0-a183-1feb977d4fc4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[contains(text(), 'Help')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[contains(text(), 'Help')]</value>
+   </webElementProperties>
+</WebElementEntity>
