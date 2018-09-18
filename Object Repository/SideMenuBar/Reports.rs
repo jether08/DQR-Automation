f0db1e6539b680b0c6ba062cc0922e12c408f74e
arcillas <arcillas@arcillas-PC>
2018-09-19 07:02:28 +0800

    ADD: Add new test case 4

---
+++ Object Repository/SideMenuBar/Reports.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Reports</name>
+   <tag></tag>
+   <elementGuidId>9a0ac1d9-11c8-47bc-8c31-aca77da4171a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[contains(text(), 'Reports')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[contains(text(), 'Reports')]</value>
+   </webElementProperties>
+</WebElementEntity>
